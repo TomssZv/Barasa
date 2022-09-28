74bff89ea6323a5ce996843baca7a197b1d33f33
jacob <tomsszvirbulis@gmail.com>
2022-09-28 20:25:49 +0300

    cocktail progress save

--- src/pages/Cocktail.js
+++ src/pages/Cocktail.js
@@ -3,12 +3,23 @@ import { useParams } from 'react-router-dom'
 
 function Cocktail() {
     const [cocktail, setCocktail] = useState([])
+    const [ingredients, setIngredients] = useState([])
     const params = useParams();
 
     useEffect(() => {
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${params.id}`)
             .then(response => response.json())
-            .then(data => setCocktail(data.drinks))
+            .then(data => {
+                setCocktail(data.drinks)
+                console.log(data.drinks)
+                let ingr = Object.keys(data.drinks[0]).filter(el => el.includes("strIngredient"))
+                for (let i = 0; i < ingr.length; ++i) {
+                    let ingred = data.drinks[0][ingr[i]]
+                    if (ingred) {
+                        setIngredients(ingredients => [...ingredients, ingred.replaceAll(' ', "%20")])
+                    }
+                }
+            })
             .catch(error => console.log(error))
     }, [])
 
@@ -17,12 +28,32 @@ function Cocktail() {
         <div id='top'>
             {cocktail ? cocktail.map((data, key) => {
                 return (
-                    <div key={key}>
-                        <h1>{data.strDrink}</h1>
+                    <div className='cocktail-cont' key={key}>
+                        <div className='cocktail-text'>
+                            <h1>{data.strDrink}</h1>
+                            <p>{data.strInstructions}</p>
+                            <h2>Ingreadients</h2>
+                            <div id='ingr-cont'>
+                                {ingredients ? ingredients.map((el, key) => {
+                                    console.log(el)
+                                    return (
+                                        <div 
+                                            key={key} 
+                                            style={{backgroundImage: `url(https://www.thecocktaildb.com/images/ingredients/${el}-Small.png)`}}
+                                            className='ingr'
+                                        >
+
+                                        </div>
+                                    )
+                                }) : <h1>Loading...</h1>}
+                            </div>
+                            
+                        </div>
                         <img src={data.strDrinkThumb} />
                     </div>
                 );
             }) : <h1>Loading...</h1>}
+            
         </div>
         <div id="bottom">
 
